fcc08a5f5d11408b45c000e7e1a3983e7224d284
kerbaal <kerbaal@yahoo.com>
2017-03-14 02:21:19 +0100

    Refactor a bit.

--- KetzerischerLootverteiler.lua
+++ KetzerischerLootverteiler.lua
@@ -4,6 +4,15 @@ local function dbgprint(...)
   --print(...)
 end
 
+function Addon:initialize()
+  Addon.ITEMS_PER_PAGE = 6
+  Addon.itemList = {}
+  Addon.fromList = {}
+  Addon.itemNum = 0
+  Addon.currentPage = 1
+  Addon.maxPages = 1
+end
+
 function Addon:getItemLink(index)
   if (index > Addon.itemNum) then
     return nil
@@ -11,12 +20,20 @@ function Addon:getItemLink(index)
   return Addon.itemList[index]
 end
 
-local function eventHandlerSystem(self, event, msg)
-  local name, roll, minRoll, maxRoll = msg:match("^(.+) würfelt. Ergebnis: (%d+) %((%d+)%-(%d+)%)$")
-  if (name and roll and minRoll and maxRoll) then
-    dbgprint (name .. roll);
-  end
+function Addon:addItem(itemString, from)
+  Addon.itemList[Addon.itemNum+1] = itemString:gsub("%s+", "")
+  Addon.fromList[Addon.itemNum+1] = from
+  Addon.itemNum = Addon.itemNum+1
 end
+
+function Addon:deleteItem(index)
+  table.remove(Addon.itemList,id)
+  table.remove(Addon.fromList,id)
+  Addon.itemNum = Addon.itemNum-1
+end
+
+
+
 
 local function getItemIdFromLink(itemLink)
   local _, _, color, Ltype, itemId, Enchant, Gem1, Gem2, Gem3, Gem4,
@@ -26,14 +43,6 @@ local function getItemIdFromLink(itemLink)
 end
 
 
-function MyLootItem_OnEnter(self, motion)
-  local itemLink = Addon:getItemLink(self:GetID())
-  if itemLink then
-	  GameTooltip:SetOwner(self, "ANCHOR_RIGHT");
-	  GameTooltip:SetHyperlink(itemLink);
-	  CursorUpdate(self);
-  end
-end
 
 local function updateButton(index)
 
@@ -143,18 +152,39 @@ local function update()
   end
 end
 
-
-local function eventHandlerItem(self, event, msg, from)
-  for itemString in string.gmatch(msg, "item[%-?%d:]+") do
-    Addon.itemList[Addon.itemNum+1] = itemString:gsub("%s+", "")
-    Addon.fromList[Addon.itemNum+1] = from
-    Addon.itemNum = Addon.itemNum+1
-  end
-
+function KetzerischerLootverteilerFrame_OnUpdate(self)
   update()
 end
 
-function KetzerischerLootverteilerFrame_OnUpdate(self)
+-- Eventhandler
+
+local function eventHandlerSystem(self, event, msg)
+  local name, roll, minRoll, maxRoll = msg:match("^(.+) würfelt. Ergebnis: (%d+) %((%d+)%-(%d+)%)$")
+  if (name and roll and minRoll and maxRoll) then
+    dbgprint (name .. roll);
+  end
+end
+
+local function eventHandlerLoot(self, event, message, sender)
+  local LOOT_SELF_REGEX = gsub(LOOT_ITEM_SELF, "%%s", "(.+)")
+  local LOOT_REGEX = gsub(LOOT_ITEM, "%%s", "(.+)")
+  local _, _, sPlayer, itemlink = string.find(message, LOOT_REGEX)
+  if not sPlayer then
+    _, _, itemlink = string.find(message, LOOT_SELF_REGEX)
+  else
+    dbgprint(sPlayer)
+  end
+  if itemlink then
+    local _, _, itemId = string.find(itemlink, "item:(%d+):")
+    dbgprint(itemlink)
+  end
+end
+
+local function eventHandlerItem(self, event, msg, from)
+  for itemString in string.gmatch(msg, "item[%-?%d:]+") do
+    Addon:addItem(itemString, from)
+  end
+
   update()
 end
 
@@ -171,11 +201,12 @@ local function eventHandler(self, event, ...)
     eventHandlerSystem(self, event, ...)
   elseif (event == "ENCOUNTER_END") then
     eventHandlerEncounterEnd(self, event, ...)
+  elseif (event == "CHAT_MSG_LOOT") then
+    eventHandlerLoot(self, event, ...)
   end
 end
 
-SLASH_KetzerischerLootverteiler1, SLASH_KetzerischerLootverteiler2 = '/klv', '/kpm';
-function SlashCmdList.KetzerischerLootverteiler(msg, editbox)
+function KetzerischerLootverteilerToggle()
   if (KetzerischerLootverteilerFrame:IsVisible()) then
     KetzerischerLootverteilerFrame:Hide()
   else
@@ -187,17 +218,18 @@ end
 BINDING_HEADER_KETZERISCHER_LOOTVERTEILER = "Ketzerischer Lootverteiler"
 BINDING_NAME_KETZERISCHER_LOOTVERTEILER_TOGGLE = "Toggle window"
 
+-- Slashcommands
+SLASH_KetzerischerLootverteiler1, SLASH_KetzerischerLootverteiler2 = '/klv', '/kpm';
+function SlashCmdList.KetzerischerLootverteiler(msg, editbox)
+  KetzerischerLootverteilerToggle()
+end
 
 function KetzerischerLootverteilerFrame_OnLoad(self)
-  Addon.ITEMS_PER_PAGE = 6
-  Addon.itemList = {}
-  Addon.fromList = {}
-  Addon.itemNum = 0
-  Addon.currentPage = 1
-  Addon.maxPages = 1
+  Addon:initialize()
   KetzerischerLootverteilerFrame:SetScript("OnEvent", eventHandler);
   KetzerischerLootverteilerFrame:RegisterEvent("CHAT_MSG_WHISPER");
   KetzerischerLootverteilerFrame:RegisterEvent("CHAT_MSG_SYSTEM");
+  KetzerischerLootverteilerFrame:RegisterEvent("CHAT_MSG_LOOT");
   KetzerischerLootverteilerFrame:RegisterEvent("ENCOUNTER_END");
 	self:RegisterForDrag("LeftButton");
 end
@@ -223,6 +255,16 @@ end
 function KetzerischerLootverteilerNavigationFrame_OnLoad()
 end
 
+
+function MyLootItem_OnEnter(self, motion)
+  local itemLink = Addon:getItemLink(self:GetID())
+  if itemLink then
+	  GameTooltip:SetOwner(self, "ANCHOR_RIGHT");
+	  GameTooltip:SetHyperlink(itemLink);
+	  CursorUpdate(self);
+  end
+end
+
 function MyLootButton_OnClick(self, button)
   dbgprint(self.itemLink)
   if (button == "LeftButton") then
@@ -236,12 +278,11 @@ function MyLootButton_OnClick(self, button)
   elseif (button == "RightButton") then
     if ( IsModifiedClick() ) then
       local id = (Addon.currentPage - 1) * Addon.ITEMS_PER_PAGE + self:GetID()
-      table.remove(Addon.itemList,id)
-      table.remove(Addon.fromList,id)
-      Addon.itemNum = Addon.itemNum-1
+      Addon:deleteItem(id)
     end
   end
 end
+
 --local _, _, Color, Ltype, Id, Enchant, Gem1, Gem2, Gem3, Gem4,
 --  Suffix, Unique, LinkLvl, reforging, Name = string.find(arg, "|?c?f?f?(%x*)|?H?([^:]*):?(%d+):?(%d*):?(%d*):?(%d*):?(%d*):?(%d*):?(%-?%d*):?(%-?%d*):?(%d*):?(%d*)|?h?%[?([^%[%]]*)%]?|?h?|?r?")
 --print("Got item" .. Id);
