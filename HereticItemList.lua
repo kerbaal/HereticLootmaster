2664b1eb84d31f4f1b03be8fdb4b5d84fc3b32e8
kerbaal <kerbaal@yahoo.com>
2017-04-12 23:05:29 +0200

    Adapt logic to HereticItem

--- HereticItemList.lua
+++ HereticItemList.lua
@@ -1,65 +1,71 @@
 HereticItem = {}
-HereticList = {}
+HereticItemList = {}
 
-HereticList.__index = HereticList
-function HereticList:New(instanceID, master)
-	local obj = {
-	instanceID = instanceID,
-	master = master,
-	entries = {},
-	}
-	setmetatable(obj, self)
-	return obj
+HereticItemList.__index = HereticItemList
+function HereticItemList:New(instanceID, master, entries)
+  local obj = {
+    instanceID = instanceID,
+    master = master,
+    entries = {},
+  }
+  setmetatable(obj, self)
+  return obj
 end
 
 HereticItem.__index = HereticItem
-function HereticItem:New(itemLink, donator, winner, rollActionID)
-	local obj = {
-	itemLink = itemLink or "",
-	donator = donator or "",
-	winner = winner or {},
-	rollActionID = rollActionID or 0,
-	}
-	setmetatable(obj, self)
-	return obj
+function HereticItem:New(itemLink, donator, sender, winner, rollActionID)
+  local obj = {
+    itemLink = itemLink,
+    donator = donator,
+    sender = sender,
+    winner = winner,
+    rollActionID = rollActionID,
+  }
+  setmetatable(obj, self)
+  return obj
 end
 
-function HereticList:GetEntry(pos)
-	if (pos < 1 or pos > #self.entries) then return nil end
-	return self.entries[pos].itemLink, self.entries[pos].donator, self.entries[pos].winner, self.entries[pos].rollActionID
+function HereticItemList:GetEntry(pos)
+  if (pos < 1 or pos > #self.entries) then return nil end
+  return self.entries[pos]
 end
 
-function HereticList:DeleteEntryAt(pos)
-	table.remove(self.entries, pos)
+function HereticItemList:DeleteEntryAt(pos)
+  table.remove(self.entries, pos)
 end
 
-function HereticList:DeleteAllEntries() 
-	for i=0, #self.entries do  
-		self.entries[i]=nil
-	end
+function HereticItemList:DeleteAllEntries()
+  for i=0, #self.entries do
+    self.entries[i]=nil
+  end
 end
 
-function HereticList:Validate()
-	setmetatable(self, HereticList)
-	if (self.instanceID == 0 or 
-		self.master == "") then
-		return false
-	elseif not self.entries then
-		self.entries = {}
-	else 
-		return true
-	end
+function HereticItemList:Validate()
+  setmetatable(self, HereticItemList)
+  if (self.instanceID == 0 or
+    self.master == "") then
+    return false
+  elseif not self.entries then
+    self.entries = {}
+  else
+    return true
+  end
 end
 
-function HereticList:GetItemLinkByID(pos)
-	return self.entries[pos].itemLink
+function HereticItemList:GetItemLinkByID(pos)
+  return self.entries[pos].itemLink
 end
 
-function HereticList:GetSize()
-	return #self.entries
+function HereticItemList:Size()
+  return #self.entries
 end
 
-function HereticList:GetEntryId(item, donator)
+function HereticItemList:AddEntry(entry)
+  table.insert(self.entries, entry)
+end
+
+
+function HereticItemList:GetEntryId(item, donator)
   for i=1, #self.entries do
     if (self.entries[i].itemLink == item and
         self.entries[i].donator == donator) then
@@ -67,10 +73,4 @@ function HereticList:GetEntryId(item, donator)
     end
   end
   return nil
-end
-
-function HereticList:AddEntry(itemLink, donator)
-	local newEntry = HereticItem:New(itemLink, donator)
-	table.insert(self.entries, newEntry)
-	return true
 end
--- KetzerischerLootverteiler.lua
+++ KetzerischerLootverteiler.lua
@@ -6,7 +6,7 @@ KetzerischerLootverteilerData = {}
 local RaidInfo = {}
 
 local function updatePageNavigation()
-  Addon.itemListView:SetNumberOfItems(Addon.itemList:GetSize())
+  Addon.itemListView:SetNumberOfItems(Addon.itemList:Size())
   local prev, next, currentPage, maxPages = Addon.itemListView:GetNavigationStatus()
   KetzerischerLootverteilerPrevPageButton:SetEnabled(prev);
   KetzerischerLootverteilerNextPageButton:SetEnabled(next);
@@ -182,7 +182,7 @@ function Addon:Initialize()
   Addon.MSG_ANNOUNCE_LOOT = "LootAnnounce"
   Addon.MSG_ANNOUNCE_LOOT_PATTERN = "^%s+([^ ]+)%s+(.*)$"
   Addon.TITLE_TEXT = "Ketzerischer Lootverteiler"
-  Addon.itemList = HereticList:New(999888777, "Nagisa-DieAldor") -- FixME hardcoded data
+  Addon.itemList = HereticItemList:New(999888777, "Nagisa-DieAldor") -- FixME hardcoded data
   Addon.itemListView = PagedView:New(Addon.ITEMS_PER_PAGE)
   Addon.master = nil;
   Addon.rolls = {};
@@ -210,7 +210,7 @@ function Addon:AddItem(itemString, from, sender)
     end
   end
 
-  Addon.itemList:AddEntry(itemString, from, sender)
+  Addon.itemList:AddEntry(HereticItem:New(itemString, from, sender))
   --PlaySound("igBackPackCoinSelect")
   PlaySound("igMainMenuOptionCheckBoxOn")
 
@@ -356,12 +356,6 @@ end
 local function eventHandlerAddonLoaded(self, event, addonName)
    if (addonName == ADDON) then
     RaidInfo:Update()
-    if KetzerischerLootverteilerData.itemList2 then
-      for i,v in pairs(KetzerischerLootverteilerData.itemList2) do
-        Addon.itemList[i] = v
-      end
-      Addon.itemList:Validate()
-    end
     if KetzerischerLootverteilerData.minRarity then
       Addon.minRarity = KetzerischerLootverteilerData.minRarity
       UIDropDownMenu_SetSelectedID(KetzerischerlootverteilerRarityDropDown, Addon.minRarity[2])
