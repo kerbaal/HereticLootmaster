acc881a88d944bdf4054165988c8820ca98e4348
kerbaal <kerbaal@yahoo.com>
2019-03-15 23:15:59 +0100

    Remove combat lockdown checks

--- KetzerischerLootverteiler.lua
+++ KetzerischerLootverteiler.lua
@@ -789,7 +789,6 @@ function HereticPlayerInfoScrollFrame_OnLoad(self)
 end
 
 function HereticLootTally_OnEnter(self)
-  if InCombatLockdown() then return end
   GameTooltip:SetOwner(self, "ANCHOR_RIGHT");
   local categories = ""
   local first = true
