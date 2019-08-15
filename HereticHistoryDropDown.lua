a87c74f418928bbbad4846bb190ca4daaa0275f0
kerbaal <kerbaal@yahoo.com>
2019-08-15 09:54:21 +0200

    Move history drop down to own file

---
+++ HereticHistoryDropDown.lua
@@ -0,0 +1,34 @@
+local ADDON, Addon = ...
+
+local Util = Addon.Util
+
+local function HereticHistoryDropDown_OnClick(self)
+  UIDropDownMenu_SetSelectedID(HereticHistoryDropDown, self:GetID())
+  Addon:SetCurrentHistory(self:GetID())
+end
+
+
+function HereticHistoryDropDown_Initialize(self, level)
+  UIDropDownMenu_SetWidth(self, 200);
+  UIDropDownMenu_JustifyText(self, "LEFT")
+  if not Addon.histories or not Addon.activeHistoryIndex or #Addon.histories < 1 then return end
+
+  for i, h in ipairs(Addon.histories or {}) do
+    local info = UIDropDownMenu_CreateInfo()
+    info.text = h.instanceName .. " " .. ((h.difficultyID and "("..Addon:DifficultyIDToString(h.difficultyID) .. ")") or "") .. " " .. (h.instanceID or "")
+    info.value = i
+    info.func = HereticHistoryDropDown_OnClick
+    UIDropDownMenu_AddButton(info, level)
+  end
+end
+
+function KetzerischerLootverteilerRarityDropDown_OnShow(self)
+  UIDropDownMenu_Initialize(self, KetzerischerLootverteilerRarityDropDown_Initialize);
+  if not Addon.minRarity then return end
+  UIDropDownMenu_SetSelectedID(self, Addon.minRarity[2])
+end
+
+function HereticHistoryDropDown_OnShow(self)
+  UIDropDownMenu_Initialize(self, HereticHistoryDropDown_Initialize);
+  UIDropDownMenu_SetSelectedID(self, Addon.activeHistoryIndex)
+end
--- KetzerischerLootverteiler.lua
+++ KetzerischerLootverteiler.lua
@@ -545,42 +545,10 @@ function HereticTab_OnClick(self)
 end
 
 
-function Addon:SetHistoryDropDown(id)
+function Addon:SetCurrentHistory(id)
   Addon.activeHistoryIndex = id
   Addon:RecomputeLootCount()
   Addon:update("change history")
-end
-
-
-local function KetzerischerLootverteilerHistoryDropDown_OnClick(self)
-  UIDropDownMenu_SetSelectedID(KetzerischerLootverteilerHistoryDropDown, self:GetID())
-  Addon:SetHistoryDropDown(self:GetID())
-end
-
-
-function KetzerischerLootverteilerHistoryDropDown_Initialize(self, level)
-  UIDropDownMenu_SetWidth(self, 200);
-  UIDropDownMenu_JustifyText(self, "LEFT")
-  if not Addon.histories or not Addon.activeHistoryIndex or #Addon.histories < 1 then return end
-
-  for i, h in ipairs(Addon.histories or {}) do
-    local info = UIDropDownMenu_CreateInfo()
-    info.text = h.instanceName .. " " .. ((h.difficultyID and "("..Addon:DifficultyIDToString(h.difficultyID) .. ")") or "") .. " " .. (h.instanceID or "")
-    info.value = i
-    info.func = KetzerischerLootverteilerHistoryDropDown_OnClick
-    UIDropDownMenu_AddButton(info, level)
-  end
-end
-
-function KetzerischerLootverteilerRarityDropDown_OnShow(self)
-  UIDropDownMenu_Initialize(self, KetzerischerLootverteilerRarityDropDown_Initialize);
-  if not Addon.minRarity then return end
-  UIDropDownMenu_SetSelectedID(self, Addon.minRarity[2])
-end
-
-function KetzerischerLootverteilerHistoryDropDown_OnShow(self)
-  UIDropDownMenu_Initialize(self, KetzerischerLootverteilerHistoryDropDown_Initialize);
-  UIDropDownMenu_SetSelectedID(self, Addon.activeHistoryIndex)
 end
 
 function KetzerischerLootverteilerRollButton_OnClick(self)
